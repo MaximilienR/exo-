3c97c3c4b1ba821de64f9cd6848f072c036dd369
MaximilienR <62236292+MaximilienR@users.noreply.github.com>
2020-06-07 23:12:58 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+
+class App extends React.Component {
+    state = {
+        name: "JB",
+       
+      
+    }
+
+    class App extends React.Component {
+        state = {
+            name: "kirikou"
+            
+        };
+
+    render ()
+    return{}
+
+    render() {
+        return (<form>
+             <p>Say something</p>
+             <alert>coucou la famille </alert>
+    <input type="text" value="" placeholder="your name"/>
+    <textarea value="" placeholder="Your message"></textarea>
+    <console.log>fdivhdvmh/<console.log>
+    <inuput type="submit" value="Comment"/>
+</form>
+    handleLastname = event => {
+        this.setState({
+            lastname: event.target.value
+        })
+    }
+
+    handleFirstname = event => {
+        this.setState({
+            firstname: event.target.value
+        })
+    }
+
+   
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+ 
+                    <input type="text" name="" id="" 
+                    placeholder="your commante " 
+                    value={this.state.age} 
+                    onChange={this.handleAge}/>
+                    
+                 
+                 <input type="submit" value="Envoyer"/>
+             </form>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));
+
+//
+class App extends React.Component {
+    state ={name :"nul"
+ 
+    }:
+render(){
+    return {
+        <div>
+ 
